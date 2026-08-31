19e0e4d191e952f8b5fb5c52794351c866356e72
Yaathvik2020 <chinnayaathvik@gmail.com>
2026-08-31 13:56:35 +0530

    Update 3-ec2.tf

--- tf-resources-ec2/3-ec2.tf
+++ tf-resources-ec2/3-ec2.tf
@@ -37,7 +37,7 @@ resource "aws_instance" "demo_ec2" {
   key_name               = var.key_name != "" ? var.key_name : null
   subnet_id               = tolist(data.aws_subnets.available.ids)[0]
   vpc_security_group_ids = [aws_security_group.demo_sg.id]
-
+  associate_public_ip_address  = true
   tags = {
      Name        = "${var.instance_name}-${terraform.workspace}"   # e.g. demo-ec2-dev
      Environment = terraform.workspace
